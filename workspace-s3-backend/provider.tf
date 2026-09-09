9a9f3c4ce28ddb4cfe114340aacc761aab747e29
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-09 23:36:58 +0530

    Add initial Terraform configuration for AWS resources

---
+++ workspace-s3-backend/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "dev"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "s3-backend-090926"
+        region = "ap-south-1"
+        shared_credentials_files = ["/root/.aws/credentials"]
+        profile = "dev"
+        key = "terraform.tfstate"
+        use_lockfile = true
+    }
+}
