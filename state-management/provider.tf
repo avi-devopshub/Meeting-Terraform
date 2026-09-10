929b15a2cf1f2a8303005138d63f0224f9f8e95d
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-10 22:42:12 +0530

    Add initial Terraform configuration for AWS EC2 instance and S3 backend

---
+++ state-management/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "dev"
+}
+
+terraform{
+    backend "s3-remote" {
+        bucket = "remote-backend-s3-10092026"
+        region = "ap-south-1"
+        profile = "dev"
+        shared_credentials_files = ["/root/.aws/credentials"]
+        key = "terraform.tfstate"
+        user_lockfile = true
+    }
+}
